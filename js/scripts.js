b0e868f52daca26c8f6b3885f86b592667e3b159
jwnardine <jwnardine@gmail.com>
2015-10-28 16:53:55 -0700

    add third test to turn I into 1; causes 2nd/1st tests to fail

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,11 @@
 var promptInput = prompt("Enter in a word");
 var justE = /e/gi;
 var justO = /o/gi;
-var leetTranslate = promptInput.replace(justE, '3').replace(justO, '0');
+var justUpperI = /I/g;
+var leetTranslate = promptInput.replace(justE, '3').replace(justO, '0').replace(justUpperI, "1");
 
 
 
-var turnLeet = function(word) {
- if (promptInput === justE); {
+var turnLeet = function(promptInput) {
    return leetTranslate;
-};
-
 };
--- spec/specs.js
+++ spec/specs.js
@@ -6,4 +6,8 @@ describe('turnLeet', function() {
   it("will replace the letter o with the number 0", function() {
     expect(turnLeet("Ionosphere")).to.equal("I0n0sph3r3");
   });
+
+  it("will replace the uppercase letter I with the number 1", function() {
+    expect(turnLeet("Ionosphere")).to.equal("10n0sph3r3");
+  });
 });
