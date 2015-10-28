3862bc9446966e0b11857ecceac6a504e3c7b3ca
jwnardine <jwnardine@gmail.com>
2015-10-28 16:06:55 -0700

    wrote and passed first test to check that Es become 3s

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var promptInput = prompt("Enter in a word");
+var justE = /e/gi;
+var leetTranslate = promptInput.replace(justE, '3');
+
+
+
+var turnLeet = function(word) {
+ if (promptInput === justE); {
+   return leetTranslate;
+ };
+};
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe('turnLeet', function() {
+  it("will replace the letter e with the number 3", function() {
+    expect(turnLeet("nice")).to.equal("nic3");
+  });
+});
