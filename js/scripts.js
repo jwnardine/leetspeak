261d8c06a363315368cff575e09e68765323ca40
jwnardine <jwnardine@gmail.com>
2015-10-28 16:42:21 -0700

    cleaned up js and fixed it so indeed test 2 passes but 1 fails

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,13 @@
 var promptInput = prompt("Enter in a word");
 var justE = /e/gi;
 var justO = /o/gi;
-var leetTranslateE = promptInput.replace(justE, '3');
-var leetTranslateO = promptInput.replace(justO, '0');
+var leetTranslate = promptInput.replace(justE, '3').replace(justO, '0');
 
 
 
 var turnLeet = function(word) {
- if (promptInput === justE === justO); {
-   return leetTranslateE;
-   return leetTranslateO;
-// };
-// if (promptInput === justO); {
-//   return leetTranslateO;
- };
+ if (promptInput === justE); {
+   return leetTranslate;
+};
+
 };
